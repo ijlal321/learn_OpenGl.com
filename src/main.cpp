dcb8184c4cb50da66acb6880d75c62509bd7fbaf
ijlal123 <ijlal.tanveer291@gmail.com>
2026-01-30 16:05:40 +0500

    added basic structure for project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <glad/glad.h> 
+// include GLAD before GLFW. The include file for GLAD includes the required OpenGL headers behind the scenes (like GL/gl.h)
+// so be sure to include GLAD before other header files that require OpenGL (like GLFW).
+#include <GLFW/glfw3.h>
+
+int main(){
+    std::cout << "Hellow world" << std::endl;
+
+
+    return 0;
+}
